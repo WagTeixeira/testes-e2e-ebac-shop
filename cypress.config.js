09d55e5f73a0e833585779157834bcce0da31b44
Wagner Alencar <wagalencar1998@outlook.com>
2023-04-04 13:58:43 -0300

    desafio 2

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+
+
+module.exports = defineConfig({
+  projectId: 'om4z95',
+
+  e2e: {
+
+   baseUrl: "http://lojaebac.ebaconline.art.br/"
+
+  },
+
+});
